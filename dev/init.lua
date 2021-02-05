2f6966c02597298ef0d8fc5d2e9889a73641f70d
s1n7ax <srineshnisala@gmail.com>
2021-02-05 16:13:00 +0530

    [ADD][DOC] add comments

--- dev/init.lua
+++ dev/init.lua
@@ -5,7 +5,11 @@ package.loaded['dev'] = nil
 package.loaded['greetings'] = nil
 package.loaded['greetings.awesome-module'] = nil
 
--- keybind to re source the lua file
+-- [ , + r ] keymap to reload the lua file
+-- NOTE: someone need to source this file to apply these configurations. So, the
+-- very first time you open the project, you have to source this file using
+-- ":luafile dev/init.lua". From that point onward, you can hit the keybind to
+-- reload
 vim.api.nvim_set_keymap('n', ',r', '<cmd>luafile dev/init.lua<cr>', {})
 
 -- keybind to test the plugin
