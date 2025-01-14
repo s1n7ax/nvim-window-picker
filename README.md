bfe49763dd297d02e5a9fda7ca252890324ef78e
Yang Jun <ouyang.qianba@gmail.com>
2025-01-14 19:10:33 +0800

    feat: add floating letter hint (#99)
    * feat: add floating letter hint
    
    * chore: fix lint error
    
    ---------
    
    Co-authored-by: s1n7ax <srineshnisala@gmail.com>

--- README.md
+++ README.md
@@ -57,9 +57,10 @@ behaviour.
 require 'window-picker'.setup({
     -- type of hints you want to get
     -- following types are supported
-    -- 'statusline-winbar' | 'floating-big-letter'
+    -- 'statusline-winbar' | 'floating-big-letter' | 'floating-letter'
     -- 'statusline-winbar' draw on 'statusline' if possible, if not 'winbar' will be
     -- 'floating-big-letter' draw big letter on a floating window
+    -- 'floating-letter' draw letter on a floating window
     -- used
     hint = 'statusline-winbar',
 
---
+++ lua/window-picker/hints/floating-letter-hint.lua
@@ -0,0 +1,94 @@
+--- @class FloatingLetterHint
+--- @field windows number[] list of window ids
+local M = {}
+
+local border = {
+	{ '╭', 'FloatBorder' },
+
+	{ '─', 'FloatBorder' },
+
+	{ '╮', 'FloatBorder' },
+
+	{ '│', 'FloatBorder' },
+
+	{ '╯', 'FloatBorder' },
+
+	{ '─', 'FloatBorder' },
+
+	{ '╰', 'FloatBorder' },
+
+	{ '│', 'FloatBorder' },
+}
+
+function M:new()
+	local o = {
+		windows = {},
+	}
+
+	setmetatable(o, self)
+	self.__index = self
+
+	return o
+end
+
+function M:set_config(config)
+	print(vim.inspect(config))
+	self.chars = config.chars
+end
+
+function M._get_float_win_pos(window, width, height)
+	local win_width = vim.api.nvim_win_get_width(window)
+	local win_height = vim.api.nvim_win_get_height(window)
+
+	local point = {
+		x = ((win_width - width) / 2),
+		y = ((win_height - height) / 2),
+	}
+
+	return point
+end
+
+function M:_show_letter_in_window(window, char)
+	local width = #char
+	local height = 1
+	local point = self._get_float_win_pos(window, width, height)
+
+	local buffer_id = vim.api.nvim_create_buf(false, true)
+	local window_id = vim.api.nvim_open_win(buffer_id, false, {
+		relative = 'win',
+		win = window,
+		focusable = true,
+		row = point.y,
+		col = point.x,
+		width = width,
+		height = height,
+		style = 'minimal',
+		border = border,
+	})
+
+	vim.api.nvim_buf_set_lines(buffer_id, 0, -1, true, { char })
+
+	return window_id
+end
+
+function M:draw(windows)
+	for index, window in ipairs(windows) do
+		local char = self.chars[index]
+		local window_id = self:_show_letter_in_window(window, char)
+		table.insert(self.windows, window_id)
+	end
+end
+
+function M:clear()
+	for _, window in ipairs(self.windows) do
+		if vim.api.nvim_win_is_valid(window) then
+			local buffer = vim.api.nvim_win_get_buf(window)
+			vim.api.nvim_win_close(window, true)
+			vim.api.nvim_buf_delete(buffer, { force = true })
+		end
+	end
+
+	self.windows = {}
+end
+
+return M
